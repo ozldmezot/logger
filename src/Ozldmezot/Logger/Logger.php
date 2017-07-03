52a2e7fbd3b2933dd25f5305b5119c5a1b9fbcc3
ozldmezot <emils.kalnins@outlook.com>
2017-07-03 16:35:57 +0300

    Removed All Static Calls
    To make Static one should use Singleton Repository

--- src/Ozldmezot/Logger/Logger.php
+++ src/Ozldmezot/Logger/Logger.php
@@ -1,42 +1,42 @@
 <?php namespace Ozldmezot\Logger;
 
 class Logger {
+	protected $base_dir;
 
-	protected static $base_dir = 'logs';
-
-	public static function setBaseDir(string $base_dir)
-	{
-		self::$base_dir = $base_dir;
-	}
-    public static function error(string $content)
+    public function __construct(string $base_dir = 'logs')
     {
-        self::log($content, 'error');
+        $this->setBaseDir($base_dir);
     }
-
-    public static function info(string $content)
+    public function setBaseDir(string $base_dir)
     {
-        self::log($content, 'info');
+        $this->base_dir = $base_dir;
     }
-
-    public static function log(string $content, string $type)
+    public function error(string $content)
     {
-		self::raw( 
+        $this->log($content, 'error');
+    }
+    public function info(string $content)
+    {
+        $this->log($content, 'info');
+    }
+    public function log(string $content, string $type)
+    {
+		$this->raw(
             date('Y-m-d H:i:s') .' '. $content . "\n\n",
 			$type.'_'.date('Y-m-d').'.log'
 		);
     }
-
-    public static function raw(string $content, string $filename)
+    public function raw(string $content, string $filename)
     {
         $parts = explode('/', $filename);
         array_pop($parts);
         $dir   = '';
-        array_unshift($parts, self::$base_dir);
+        array_unshift($parts, $this->base_dir);
         foreach($parts as $part)
             if(!is_dir($dir .= "$part/")) {
                 mkdir($dir, 0755);
         }
-
-        file_put_contents(self::$base_dir. '/'.$filename, $content, FILE_APPEND);
+        file_put_contents($this->base_dir. '/'.$filename, $content, FILE_APPEND);
     }
 }
+
